e8b1060e72901ce60fe924b4f526163d15913097
Mikeoduoli <mikeoduoli@gmail.com>
2024-04-12 08:43:16 +0300

    Environment setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      Hello world!
+    </h1>
+    
+    </>
   )
 }
 
