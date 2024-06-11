49abd1369e79a642a57e0ba385c5d77d00a1d76e
Mikeoduoli <mikeoduoli@gmail.com>
2024-06-11 09:10:28 +0300

    Creating a Product Catalogue

--- src/data/catalogue.jsx
+++ src/data/catalogue.jsx
@@ -1,3 +1,14 @@
 catalogue = [{
-    hoodie: ''
+    hood: 'Hoodies',
+    price: 'Ksh. 2500.00',
+    size: ["S", "M", "L", "XL", "XXL",]
+},
+
+{
+    t_shirts: "T-shirts",
+    price_t: "Ksh. 850",
+    size: ["S", "M", "L", "XL", "XXL",]
+},
+{
+
 }]
